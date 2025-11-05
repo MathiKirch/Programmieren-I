06e830123d59f8e4a4efaa7c656ea8472518d240
MathiKirch <mathis.kirchhoff@gmx.net>
2025-11-05 19:41:28 +0100

    Chapter 3, declaration of vars and consts etc

---
+++ src/lecture1/chapter3/datatypesexam.java
@@ -0,0 +1,15 @@
+package lecture.chapter3;
+
+public class datatypesexam {
+
+    int numberA;
+
+    //initialisation of numbA
+    numberA = 123;
+
+    long  numbB = 123;
+
+    int numberC, numberD = 10, numberE;
+    numberC = 100;
+    System.out.println(numberC);
+}
