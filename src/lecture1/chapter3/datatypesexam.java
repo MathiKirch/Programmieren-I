7ad5268ef8c6f7d29b7289e359eeb18e0b97ab73
MathiKirch <mathis.kirchhoff@gmx.net>
2025-11-12 16:20:47 +0100

    Erstmal commiten 12.11.

--- src/lecture1/chapter3/datatypesexam.java
+++ src/lecture1/chapter3/datatypesexam.java
@@ -13,7 +13,7 @@ public class datatypesexam {
         numberC = 100;
         System.out.println(numberC);
 
-        final double PI = 3.14;
+        final double PI = 3.149;
 
         int literalExample = 011; //octal literal --> 011 -> 9
         System.out.println(literalExample);
@@ -25,5 +25,22 @@ public class datatypesexam {
         System.out.println(literalExample);
 
         long longNumber = 3_000_000_000L; //suffix L/l ist wichtig wenn man long als var type nimmt
+
+        //float / double literal
+        double literalexample = 5.0;
+
+        literalexample = .567;
+        literalexample = -0.5e-2; // e steht für mal 10 hoch und dann die zahl, also exponent
+        // hier also mal zehn hoch minus 2
+        System.out.println(literalexample);
+
+        float literalExampleC = 0.5f; //für float ist ein f am ende zwingend erforderlich, ansonstemn wird als double interpretiert
+
+        char literalexampled = 'a';//einfache hochkomma und exakt ein zeichen
+        System.out.println(literalexampled);
+
+        String literalexampletext = "Test \"scheiss Appel\" Text";//wichtig ist doppeltes hochkomma, mehrere zeichen
+        // Backslash lässt das nächste zeichen als einfaches zeichen, nicht als anweisung interpretieren, nennt man escape sequenz
+        System.out.println(literalexampletext);
     }
 }
